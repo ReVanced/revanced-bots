cb4dc42dfab8cf9821b03316cf56b405abd497ae
PalmDevs <git@palmdevs.me>
2025-04-14 21:08:21 +0700

    fix(bots/discord): fix sticky msg force timer always starting, add more logging

--- bots/discord/src/events/discord/messageCreate/stickyMessageReset.ts
+++ bots/discord/src/events/discord/messageCreate/stickyMessageReset.ts
@@ -7,18 +7,22 @@ withContext(on, 'messageCreate', async ({ discord, logger }, msg) => {
     const store = discord.stickyMessages[msg.guildId]?.[msg.channelId]
     if (!store) return
 
-    // (Re)start the timer
-    store.timerActive = true
-    if (store.timer) store.timer.refresh()
-    else store.timer = setTimeout(store.send, store.timerMs) as NodeJS.Timeout
-
-    // Timer is already active, and force timer isn't active, so we start the latter
+    // Timer is already active from previous event, and force timer isn't active, so we start the latter
     if (store.timerActive && store.forceTimerMs && !store.forceTimerActive) {
-        logger.debug(`Channel ${msg.channelId} in guild ${msg.guildId} is active, starting force send timer`)
+        logger.debug(
+            `Channel ${msg.channelId} in guild ${msg.guildId} is very active, starting sticky message force timer`,
+        )
 
         // (Re)start the force timer
         store.forceTimerActive = true
         if (store.forceTimer) store.forceTimer.refresh()
         else store.forceTimer = setTimeout(store.send, store.forceTimerMs)
     }
+
+    logger.debug(`Channel ${msg.channelId} in guild ${msg.guildId} is active, starting sticky message timer`)
+
+    // (Re)start the timer
+    store.timerActive = true
+    if (store.timer) store.timer.refresh()
+    else store.timer = setTimeout(store.send, store.timerMs) as NodeJS.Timeout
 })

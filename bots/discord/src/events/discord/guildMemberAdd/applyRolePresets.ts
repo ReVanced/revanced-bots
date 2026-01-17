77626c9aeddceb25c2f0d418bf764efc66669baf
PalmDevs <git@palmdevs.me>
2026-01-17 21:13:17 +0700

    fix(bots/discord): more logging during apply role preset on member join
    It seems to keep skipping some members for some reason...

--- bots/discord/src/events/discord/guildMemberAdd/applyRolePresets.ts
+++ bots/discord/src/events/discord/guildMemberAdd/applyRolePresets.ts
@@ -2,6 +2,7 @@ import { and, eq, gt } from 'drizzle-orm'
 import { appliedPresets } from '$/database/schemas'
 import { on, withContext } from '$/utils/discord/events'
 import { applyRolesUsingPreset } from '$/utils/discord/rolePresets'
+import { logger } from '$/context'
 
 withContext(on, 'guildMemberAdd', async ({ database }, member) => {
     const applieds = await database.query.appliedPresets.findMany({
@@ -11,6 +12,13 @@ withContext(on, 'guildMemberAdd', async ({ database }, member) => {
             gt(appliedPresets.until, Date.now() / 1000),
         ),
     })
+
+    if (!applieds.length) return
+
+    logger.info(
+        `Re-applying role presets for member ${member.id} in guild ${member.guild.id}:`,
+        applieds.map(x => x.preset),
+    )
 
     for (const { preset } of applieds) await applyRolesUsingPreset(preset, member)
 })

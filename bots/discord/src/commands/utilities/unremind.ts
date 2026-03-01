d41b4732f8bbc4d49172c6ccf8a3bec50e169aa3
PalmDevs <git@palmdevs.me>
2026-03-01 18:32:46 +0700

    fix(bots/discord): utility commands not letting users pass requirements

--- bots/discord/src/commands/utilities/unremind.ts
+++ bots/discord/src/commands/utilities/unremind.ts
@@ -12,6 +12,7 @@ export default new Command({
     type: Command.Type.ChatGuild,
     requirements: {
         roles: config.utilities?.roles,
+        mode: 'any',
     },
     options: {
         id: {
